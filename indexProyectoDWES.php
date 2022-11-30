4271fb450b10f930a45c98081349c3a98b00d28d
David Aparicio Sir <davidapariciosir@gmail.com>
2022-11-30 11:57:36 +0100

    30/11/2022 Initial Commit

---
+++ indexProyectoDWES.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>David Aparicio DWES</title>
+        <link rel="stylesheet" href="webroot/css/estilos.css"/>
+        <link rel="icon" type="image/x-icon" href="doc/img/favicon.ico"/>
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+            <div id="nav">
+                <h2>Temas</h2>
+                <ul>
+                    <li><a href="doc/EstudioTema1.pdf" target="_blank">Tema 1 Desarrollo Web en Entorno Servidor</a></li>
+                    <li><a href="doc/EstudioTema2.pdf" target="_blank">Tema 2 Instalación,configuracion y documentacion del entorno de desarrollo y del entorno de explotación</a></li>
+                    <li><a href="../201DWESProyectoTema3/indexProyectoTema3.php">Tema 3 Características del Lenguaje PHP</a></li>
+                    <li><a href="../201DWESProyectoTema4/indexProyectoTema4.php">Tema 4 Tecnicas de Acceso a Datos en PHP</a></li>
+                    <li><a href="../201DWESProyectoTema5/indexProyectoTema5.php">Tema 5 Desarrollo de Aplicaciones Web utilizando código embebido</a></li>
+                    <li><a href="../201DWESProyectoLoginLogoffTema5/codigoPHP/login.php">Tema 5 Proyecto Login Logoff</a></li>
+                    <li><a>Tema 6 Programacion Orientada a Objetos en PHP</a></li>
+                    <li><a>Tema 7 Programación de Servicios Web</a></li>
+                    <li><a>Tema 8 Desarrolo de Aplicaciones web Híbridas</a></li> 
+                    </ul> 
+            </div> 
+        </header>
+        <footer>
+            <a href=".."><img src="doc/img/home.png" alt="HOME"/></a>
+            <a href="../doc/CVDavidAparicioSir.pdf" target="blank"><img src="doc/img/cv.png" alt="CV David Aparicio"/></a>
+            <a href="https://www.github.com/davidas02" target="blank"><img src="doc/img/git.png" alt="github David Aparicio"/></a>
+            <p>2022-2023 David Aparicio Sir &COPY; Todos los derechos reservados</p>
+        </footer>
+    </body>
+</html>
